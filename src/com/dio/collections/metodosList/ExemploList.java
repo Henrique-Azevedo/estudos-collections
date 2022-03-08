8d693fae8247987d2c4cc335870123ffff97aa2a
Henrique-Azevedo <henrique.azevedo.pro@gmail.com>
2022-03-08 06:33:56 -0300

    atutal b

--- src/com/dio/collections/metodosList/ExemploList.java
+++ src/com/dio/collections/metodosList/ExemploList.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class ExemploList {
 
-    public static void main(String[] args) {
-//Dada uma lista com 7 notas de uma aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
+   public static void main(String[] args) {}
+/*Dada uma lista com 7 notas de uma aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
             //formas de utilização:
         //List notas = ArrayList(); => esta forma era feita antes do java 5
         //List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
@@ -13,14 +13,12 @@ public class ExemploList {
         //Este tipo de implementação não permite adicionar e nem remover ítens na lista
         //List<Double> notas = (Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
         //notas.add(10d);
-        /*notas.remove(5d);
-        System.out.println(notas);*/
 
         //Este tipo de implementação torna e lista imutável
-        /*List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+        List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
         notas.add(1d);
         notas.remove(5d);
-        System.out.println(notas);*/
+        System.out.println(notas);
 
         System.out.println("Crie um lista e adicione as sete notas: ");
 
@@ -47,8 +45,8 @@ public class ExemploList {
 
         System.out.println("confita se a nota 5.0 está na lista: " + notas.contains(5d));
 
-        /*System.out.println("Exiba todas as notas na ordem que foram informadas: ");
-        for (Double nota : notas) System.out.println(nota);*/
+        System.out.println("Exiba todas as notas na ordem que foram informadas: ");
+        for (Double nota : notas) System.out.println(nota);
 
         System.out.println("Exima a terceira nota adicionada: " + notas.get(2));
         System.out.println(notas.toString());
@@ -83,13 +81,13 @@ public class ExemploList {
         }
         System.out.println(notas);
 
-        /*System.out.println("Apague toda a lista");
+        System.out.println("Apague toda a lista");
         notas.clear();
-        System.out.println(notas);*/
+        System.out.println(notas);
 
-        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
+       //System.out.println("Confira se a lista está vazia: " + notas.isEmpty());*/
 
-        //Resolva as questões abaixo utilizando os métoodos da implementação LinkedList:
+        /*Resolva as questões abaixo utilizando os métoodos da implementação LinkedList:
 
         System.out.println("Crie uma lista chamada notas2 " +
                            "e coloque todos os elementos  da list ArrayList nessa nova lista: ");
@@ -106,5 +104,5 @@ public class ExemploList {
         System.out.println(notas2);
 
 
-    }
+    }*/
 }
